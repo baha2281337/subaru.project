c815dc0f60fa7955947b377a95b23337cb7d150e
Baha <baha2281337@gmail.com>
2026-03-04 02:01:08 +0600

    delal

--- cars/admin.py
+++ cars/admin.py
@@ -3,6 +3,6 @@ from .models import Car
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ("title", "year", "price", "is_available")
+    list_display = ("title", "year", "price",)
     list_filter = ("is_available", "year")
     search_fields = ("title", "description")
